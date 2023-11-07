11c9ea5a8a434babe6dce2bb8376b6ecb5e7b6f8
teofoli-matteo <114015283+teofoli-matteo@users.noreply.github.com>
2023-11-07 09:51:09 +0100

    Update dispatcher.php

--- src/Dispatcher/dispatcher.php
+++ src/Dispatcher/dispatcher.php
@@ -1,1 +1,22 @@
+<?php
 
+// namespace iutnc\deefy\dispatch;
+ // use iutnc\deefy\action\SigninAction;
+
+class Dispatcher {
+    private $action;
+
+    public function __construct($action) {
+        $this->action = $action;
+    }
+
+    public function run(): void {
+      default:
+                $this->renderPage(file_get_contents('index.html'));
+                break;
+    }
+
+  private function renderPage(string $html): void {
+        echo $html;
+    }
+}
