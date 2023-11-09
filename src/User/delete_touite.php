7f0415b599a9dea119798547c35fa7c4c0c5e1d3
Alex Collin <93080448+SkyPurl@users.noreply.github.com>
2023-11-09 20:59:29 +0100

    Update delete_touite.php

--- src/User/delete_touite.php
+++ src/User/delete_touite.php
@@ -1,1 +1,22 @@
+<?php
 
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["idTouite"])) {
+        $idTouite = $_POST["idTouite"];
+
+        try {
+            $pdo = connexionFactory::makeConnection();
+            $stmt = $pdo->prepare("DELETE FROM touite WHERE idTouite = :idTouite");
+            $stmt->bindParam(':idTouite', $idTouite);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            echo "Erreur lors de la suppression du touite: " . $e->getMessage();
+            exit();
+        }
+
+        // Actualiser la page après la suppression
+        echo '<script>refreshPage();</script>';
+    }
+}
+?>
